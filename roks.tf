3fb7e7fc06cc6d02b242c32a29b75836f2e460b7
René Meyer <>
2021-02-01 13:52:37 +0100

    roks

--- roks.tf
+++ roks.tf
@@ -3,7 +3,6 @@ resource "ibm_resource_instance" "cos_instance" {
   service  = "cloud-object-storage"
   plan     = "standard"
   location = "global"
-  resource_group = data.ibm_resource_group.group.id
 }
 
 resource "ibm_container_vpc_cluster" "cluster" {
