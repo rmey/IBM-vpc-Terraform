4c0cb9629560eb39bcb995b466375fe7ec714d8e
René Meyer <>
2021-02-01 12:20:34 +0100

    test

--- main.tf
+++ main.tf
@@ -42,27 +42,27 @@ resource "ibm_is_vpc_address_prefix" "prefix3" {
 #---------------------------------------------------------
 # Get Public Gateway's for Zone 1
 #---------------------------------------------------------
-#resource "ibm_is_public_gateway" "pubgw-zone1" {
-#  name = "${var.vpc-name}-${var.zone1}-pubgw"
-#  vpc  = ibm_is_vpc.vpc1.id
-#  zone = var.zone1
-#}
+resource "ibm_is_public_gateway" "pubgw-zone1" {
+  name = "${var.vpc-name}-${var.zone1}-pubgw"
+  vpc  = ibm_is_vpc.vpc1.id
+  zone = var.zone1
+}
 #---------------------------------------------------------
 # Get Public Gateway's for Zone 2
 #---------------------------------------------------------
-#resource "ibm_is_public_gateway" "pubgw-zone2" {
-#  name = "${var.vpc-name}-${var.zone2}-pubgw"
-#  vpc  = ibm_is_vpc.vpc1.id
-#  zone = var.zone2
-#}
+resource "ibm_is_public_gateway" "pubgw-zone2" {
+  name = "${var.vpc-name}-${var.zone2}-pubgw"
+  vpc  = ibm_is_vpc.vpc1.id
+  zone = var.zone2
+}
 #---------------------------------------------------------
 # Get Public Gateway's for Zone 3
 #---------------------------------------------------------
-#resource "ibm_is_public_gateway" "pubgw-zone3" {
-#  name = "${var.vpc-name}-${var.zone2}-pubgw"
-#  vpc  = ibm_is_vpc.vpc1.id
-#  zone = var.zone3
-#}
+resource "ibm_is_public_gateway" "pubgw-zone3" {
+  name = "${var.vpc-name}-${var.zone2}-pubgw"
+  vpc  = ibm_is_vpc.vpc1.id
+  zone = var.zone3
+}
 
 #---------------------------------------------------------
 ## Create subnet in Zone1
@@ -72,7 +72,7 @@ resource "ibm_is_subnet" "server-subnet-zone1" {
   vpc             = ibm_is_vpc.vpc1.id
   zone            = var.zone1
   ipv4_cidr_block = var.server-subnet-zone-1
-  #public_gateway  = ibm_is_public_gateway.pubgw-zone1.id
+  public_gateway  = ibm_is_public_gateway.pubgw-zone1.id
 }
 
 #---------------------------------------------------------
@@ -83,7 +83,7 @@ resource "ibm_is_subnet" "server-subnet-zone2" {
   vpc             = ibm_is_vpc.vpc1.id
   zone            = var.zone2
   ipv4_cidr_block = var.server-subnet-zone-2
-  #public_gateway  = ibm_is_public_gateway.pubgw-zone2.id
+  public_gateway  = ibm_is_public_gateway.pubgw-zone2.id
 }
 
 #---------------------------------------------------------
@@ -94,5 +94,5 @@ resource "ibm_is_subnet" "server-subnet-zone3" {
   vpc             = ibm_is_vpc.vpc1.id
   zone            = var.zone3
   ipv4_cidr_block = var.server-subnet-zone-3
-  #public_gateway  = ibm_is_public_gateway.pubgw-zone3.id
+  public_gateway  = ibm_is_public_gateway.pubgw-zone3.id
 }
