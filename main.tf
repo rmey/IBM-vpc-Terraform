c99e3d7d3786dd85fb253edf35a3c10d3b39b407
René Meyer <>
2021-02-01 10:39:33 +0100

    test

--- main.tf
+++ main.tf
@@ -6,13 +6,6 @@ data "ibm_resource_group" "group" {
 }
 
 #---------------------------------------------------------
-# Get VSI image id
-#---------------------------------------------------------
-data ibm_is_image "select_image" {
-  name = var.select_image_name
-}
-
-#---------------------------------------------------------
 # Create new VPC
 #---------------------------------------------------------
 resource "ibm_is_vpc" "vpc1" {
@@ -31,7 +24,6 @@ resource "ibm_is_vpc_address_prefix" "prefix1" {
   cidr = var.vpc-address-prefix
 }
 
-
 #---------------------------------------------------------
 # Get Public Gateway's for Zone 1
 #---------------------------------------------------------
@@ -40,10 +32,25 @@ resource "ibm_is_public_gateway" "pubgw-zone1" {
   vpc  = ibm_is_vpc.vpc1.id
   zone = var.zone1
 }
-
+#---------------------------------------------------------
+# Get Public Gateway's for Zone 2
+#---------------------------------------------------------
+resource "ibm_is_public_gateway" "pubgw-zone2" {
+  name = "${var.vpc-name}-${var.zone2}-pubgw"
+  vpc  = ibm_is_vpc.vpc1.id
+  zone = var.zone2
+}
+#---------------------------------------------------------
+# Get Public Gateway's for Zone 3
+#---------------------------------------------------------
+resource "ibm_is_public_gateway" "pubgw-zone3" {
+  name = "${var.vpc-name}-${var.zone2}-pubgw"
+  vpc  = ibm_is_vpc.vpc1.id
+  zone = var.zone3
+}
 
 #---------------------------------------------------------
-## Create Webapp Subnets in Zone1
+## Create Webapp in Zone1
 #---------------------------------------------------------
 resource "ibm_is_subnet" "server-subnet-zone1" {
   name            = "${var.vpc-name}-${var.zone1}-server"
@@ -51,4 +58,26 @@ resource "ibm_is_subnet" "server-subnet-zone1" {
   zone            = var.zone1
   ipv4_cidr_block = var.server-subnet-zone-1
   public_gateway  = ibm_is_public_gateway.pubgw-zone1.id
+}
+
+#---------------------------------------------------------
+## Create Webapp in Zone2
+#---------------------------------------------------------
+resource "ibm_is_subnet" "server-subnet-zone2" {
+  name            = "${var.vpc-name}-${var.zone2}-server"
+  vpc             = ibm_is_vpc.vpc1.id
+  zone            = var.zone2
+  ipv4_cidr_block = var.server-subnet-zone-2
+  public_gateway  = ibm_is_public_gateway.pubgw-zone2.id
+}
+
+#---------------------------------------------------------
+## Create Webapp in Zone3
+#---------------------------------------------------------
+resource "ibm_is_subnet" "server-subnet-zone3" {
+  name            = "${var.vpc-name}-${var.zone3}-server"
+  vpc             = ibm_is_vpc.vpc1.id
+  zone            = var.zone3
+  ipv4_cidr_block = var.server-subnet-zone-3
+  public_gateway  = ibm_is_public_gateway.pubgw-zone3.id
 }
--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "resource_group" {
 #---------------------------------------------------------
 ## DEFINE Region and Zones in US-South Dallas or
 ## Frankfurt region eu-de
-## To switch regin via CLI:  $ibmcloud target -r us-south  
+## To switch regin via CLI:  $ibmcloud target -r us-south
 ## or   ibmcloud target -r eu-de
 #---------------------------------------------------------
 # cloud region like eu-de (GEN1) or us-south (GEN2)
@@ -80,92 +80,6 @@ variable "server-subnet-zone-3" {
   default = "172.21.6.0/24"
 }
 
-#---------------------------------------------------------
-## DEFINE OS image to be used for compute instances
-## The image types are different between Gen1 and Gen2
-## You can generate the full list via CLI   $ibmcloud is images
-## To switsch between both genaration via CLI   $ibmcloud is target --gen 2
-## or   $ibmcloud is target --gen 1
-##
-##        GEN 1                                             GEN 2
-##    "ibm-ubuntu-16-04-6-minimal-amd64-1 "               = "ibm-ubuntu-16-04-5-minimal-amd64-1 "
-##    "ibm-windows-server-2012-r2-full-standard-amd64-1 " =  actual not available
-##    "ibm-windows-server-2012-full-standard-amd64-1"     =  actual not available
-##    "ibm-centos-7-6-minimal-amd64-1 "                   = "ibm-centos-7-0-64"
-##    "ibm-ubuntu-18-04-2-minimal-amd64-1"                = "ibm-centos-7-6-minimal-amd64-1 "
-##    "ibm-debian-9-9-minimal-amd64-1 "                   = "ibm-debian-9-9-minimal-amd64-1 "
-#---------------------------------------------------------
-variable "select_image_name" {
-  description = "use  GEN1 ubuntu-16.04-amd64  or GEN2  ibm-ubuntu-18-04-1-minimal-amd64-1"
-  default = "ibm-ubuntu-18-04-1-minimal-amd64-1"
-}
-
-#---------------------------------------------------------
-## DEFINE compute instance profile & quantity
-## The image types are different between Gen1 and Gen2
-## You can generate this list via CLI   $ibmcloud is instance-profiles
-##
-##        GEN 1        GEN 2
-##   "bc1-16x64"   = "bx2-16x64"
-##    "bc1-2x8"    = "bx2-2x8"
-##    "bc1-32x128" = "bx2-32x128"
-##    "bc1-48x192" = "bx2-48x192"
-##    "bc1-4x16"   = "bx2-4x16"
-##    "bc1-8x32"   = "bx2-8x32"
-##    "cc1-16x32"  = "cx2-16x32"
-##    "cc1-2x4"    = "cx2-2x4"
-##    "cc1-32x64"  = "cx2-32x64"
-##    "cc1-4x8"    = "cx2-4x8"
-##    "cc1-8x16"   = "cx2-8x16"
-##    "mc1-16x128" = "mx2-16x128"
-##    "mc1-2x16"   = "mx2-2x16"
-##    "mc1-32x256" = "mx2-32x256"
-##   "mc1-4x32"    = "mx2-4x32"
-##    "mc1-8x64"   = "mx2-8x64"
-##
-## plus power server within GEN 2 like:
-##     "bp2-2x8"
-##     "cp2-2x4"
-##     "gp2-24x224x2"
-##     "mp2-2x16"
-#---------------------------------------------------------
-variable "profile-server" {
-  description = "use  GEN1 cc1-2x4  or GEN2  cx2-2x4"
-  default = "cx2-2x4"
-}
-
-variable "server-name" {
-  default = "servername"
-}
-
-variable "server-count" {
-  default = 1
-}
-
 variable "ibmcloud_timeout" {
    default = 300
 }
-
-#---------------------------------------------------------
-## Create VPN Connection 
-#---------------------------------------------------------
-variable "vpn_gw"{
-  default = "vpngw1"
-}
-
-variable "vpn_gw_con"{
-  default = "vpngwcon1"
-}
-
-variable "peer_adress"{
-  default = "1.2.3.4"
-}
-
-variable "preshared_key" {
-  default = "DEADBEEF"
-}
-
-variable "peer_ipv4_cidr_block"{
-  default = ["192.168.178.0/24"]
-}
-
