3a22382d712b52938677b416f7a737bf5cad9c1d
René Meyer <>
2021-02-02 13:38:38 +0100

    cos

--- roks.tf
+++ roks.tf
@@ -4,24 +4,24 @@ data "ibm_resource_instance" "cos_instance" {
   location = "global"
 }
 
-#resource "ibm_container_vpc_cluster" "cluster" {
-#  name              = var.cluster_name
-#  vpc_id            = ibm_is_vpc.vpc1.id
-#  kube_version      = var.cluster_kube_version
-#  flavor            = var.cluster_flavor
-#  worker_count      = var.cluster_worker_count
-#  cos_instance_crn  = data.ibm_resource_instance.cos_instance.id
-#  resource_group_id = data.ibm_resource_group.group.id
-#  zones {
-#          subnet_id = ibm_is_subnet.server-subnet-zone1.id
-#          name      = var.zone1
-#        }
-#  zones {
-#          subnet_id = ibm_is_subnet.server-subnet-zone2.id
-#          name      = var.zone2
-#        }
-#  zones {
-#          subnet_id = ibm_is_subnet.server-subnet-zone2.id
-#          name      = var.zone2
-#        }
-#}
+resource "ibm_container_vpc_cluster" "cluster" {
+  name              = var.cluster_name
+  vpc_id            = ibm_is_vpc.vpc1.id
+  kube_version      = var.cluster_kube_version
+  flavor            = var.cluster_flavor
+  worker_count      = var.cluster_worker_count
+  cos_instance_crn  = data.ibm_resource_instance.cos_instance.id
+  resource_group_id = data.ibm_resource_group.group.id
+  zones {
+          subnet_id = ibm_is_subnet.server-subnet-zone1.id
+          name      = var.zone1
+        }
+  zones {
+          subnet_id = ibm_is_subnet.server-subnet-zone2.id
+          name      = var.zone2
+        }
+  zones {
+          subnet_id = ibm_is_subnet.server-subnet-zone3.id
+          name      = var.zone3
+        }
+}
