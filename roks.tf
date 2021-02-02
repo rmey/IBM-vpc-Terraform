e60b30162576fe138376975646bfaee87d1a73c8
René Meyer <>
2021-02-02 13:26:48 +0100

    cos

--- roks.tf
+++ roks.tf
@@ -11,7 +11,7 @@ data "ibm_resource_instance" "cos_instance" {
 #  flavor            = var.cluster_flavor
 #  worker_count      = var.cluster_worker_count
 #  cos_instance_crn  = data.ibm_resource_instance.cos_instance.id
-  resource_group_id = data.ibm_resource_group.group.id
+#  resource_group_id = data.ibm_resource_group.group.id
 #  zones {
 #          subnet_id = ibm_is_subnet.server-subnet-zone1.id
 #          name      = var.zone1
