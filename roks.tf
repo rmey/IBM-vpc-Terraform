886fe0a214177a6b6e7fa515abab54214b060a0c
René Meyer <>
2021-02-02 09:45:06 +0100

    cos

--- roks.tf
+++ roks.tf
@@ -6,25 +6,17 @@ resource "ibm_container_vpc_cluster" "cluster" {
   worker_count      = var.cluster_worker_count
   cos_instance_crn  = var.cos_instance_crn
   resource_group_id = data.ibm_resource_group.group.id
-  #zones = [
-  #  {
-  #    subnet_id = "0717-0c0899ce-48ac-4eb6-892d-4e2e1ff8c9478"
-  #    name      = "us-south-1"
-  #  }
-  #]
-
-  #zones = [
-      #{
-      #  subnet_id = ibm_is_subnet.server-subnet-zone1.id
-      #  name      = var.zone1
-      #},
-      #{
-      #  subnet_id = ibm_is_subnet.server-subnet-zone2.id
-      #  name      = var.zone2
-      #},
-      #{
-      #  subnet_id = ibm_is_subnet.server-subnet-zone3.id
-      #  name      = var.zone3
-      #}
+  zones{
+        subnet_id = ibm_is_subnet.server-subnet-zone1.id
+        name      = var.zone1
+       }
+  #    {
+  #      subnet_id = ibm_is_subnet.server-subnet-zone2.id
+  #      name      = var.zone2
+  #    },
+  #    {
+  #      subnet_id = ibm_is_subnet.server-subnet-zone3.id
+  #      name      = var.zone3
+  #    }
   #]
 }
