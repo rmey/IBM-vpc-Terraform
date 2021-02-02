df92d32c698a17a85a5042571f2b14b2a5c4032b
René Meyer <>
2021-02-02 10:17:22 +0100

    cos

--- roks.tf
+++ roks.tf
@@ -6,7 +6,7 @@ resource "ibm_container_vpc_cluster" "cluster" {
   worker_count      = var.cluster_worker_count
   cos_instance_crn  = var.cos_instance_crn
   resource_group_id = data.ibm_resource_group.group.id
-  zones = {
+  zones {
         subnet_id = ibm_is_subnet.server-subnet-zone1.id
         name      = var.zone1
        }
