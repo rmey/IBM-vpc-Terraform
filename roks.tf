9b2d738acad579e391e8eb4703d017e248ff8514
René Meyer <>
2021-02-02 09:36:03 +0100

    cos

--- roks.tf
+++ roks.tf
@@ -7,17 +7,24 @@ resource "ibm_container_vpc_cluster" "cluster" {
   cos_instance_crn  = var.cos_instance_crn
   resource_group_id = data.ibm_resource_group.group.id
   zones = [
-      {
-        subnet_id = ibm_is_subnet.server-subnet-zone1.id
-        name      = var.zone1
-      },
-      {
-        subnet_id = ibm_is_subnet.server-subnet-zone2.id
-        name      = var.zone2
-      },
-      {
-        subnet_id = ibm_is_subnet.server-subnet-zone3.id
-        name      = var.zone3
-      }
+    {
+      subnet_id = "0717-0c0899ce-48ac-4eb6-892d-4e2e1ff8c9478"
+      name      = "us-south-1"
+    }
   ]
+
+  #zones = [
+      #{
+      #  subnet_id = ibm_is_subnet.server-subnet-zone1.id
+      #  name      = var.zone1
+      #},
+      #{
+      #  subnet_id = ibm_is_subnet.server-subnet-zone2.id
+      #  name      = var.zone2
+      #},
+      #{
+      #  subnet_id = ibm_is_subnet.server-subnet-zone3.id
+      #  name      = var.zone3
+      #}
+  #]
 }
